ff82b3f128aae5ae4a2e44afc522e5f9dd55fd93
aliyahj <41165931+aliyahj@users.noreply.github.com>
2018-07-23 15:09:28 -0400

    reset

--- scorekeeper.js
+++ scorekeeper.js
@@ -13,6 +13,7 @@ p1Button.addEventListener("click" , function(){
 	if(!gameOver){
 p1Score++ ;
 if(p1Score === winningScore) {
+	p1Display.classList.add("winner");
 	gameOver = true ;
 }
 p1Display.textContent = p1Score ;
@@ -24,9 +25,20 @@ p2Button.addEventListener("click" , function(){
 	if(!gameOver){
 p2Score++ ;
 if(p2Score === winningScore) {
+	p2Display.classList.add("winner");
 	gameOver = true
 }
 p2Display.textContent = p2Score ;		
 	}
+
+});
+
+resetButton.addEventListener("click", function(){
+p1Score = 0 ;
+p2Score = 0 ;
+p1Display.textContent = 0 ;
+p2Display.textContent = 0 ;
+p1Display.classList.remove("winner");
+p2Display.classList.remove("winner");
 
 });
