9915b9be871db4525daf61f6327f24ecf085ebd4
aliyahj <41165931+aliyahj@users.noreply.github.com>
2018-07-23 14:30:38 -0400

    kjaa

--- scorekeeper.js
+++ scorekeeper.js
@@ -4,10 +4,20 @@ var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var p1Score = 0 ; 
 var p2Score = 0 ;
+var gameOver = false ;
+var winningScore = 5 ;
+
+
 
 p1Button.addEventListener("click" , function(){
+	if(!gameOver){
 p1Score++ ;
+if(p1Score === winningScore) {
+	gameOver = true ;
+}
 p1Display.textContent = p1Score ;
+	}
+
 });
 
 p2Button.addEventListener("click" , function(){
